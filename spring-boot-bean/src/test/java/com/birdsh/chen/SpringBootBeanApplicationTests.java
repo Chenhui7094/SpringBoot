e7b0ff5098a5e0f9c680a42b0c6ea213ff8ec5b2
chenhuihui <chenhuihui@birdsh.com>
2017-09-26 16:19:18 +0800

    2

---
+++ spring-boot-bean/src/test/java/com/birdsh/chen/SpringBootBeanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.birdsh.chen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
