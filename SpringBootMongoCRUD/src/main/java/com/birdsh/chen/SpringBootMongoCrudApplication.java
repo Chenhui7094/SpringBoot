dd251ff3e0ab5c38781d054b00f61e439185dadb
chenhuihui <chenhuihui@birdsh.com>
2017-09-28 10:52:14 +0800

    ss

---
+++ SpringBootMongoCRUD/src/main/java/com/birdsh/chen/SpringBootMongoCrudApplication.java
@@ -0,0 +1,12 @@
+package com.birdsh.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoCrudApplication.class, args);
+	}
+}
