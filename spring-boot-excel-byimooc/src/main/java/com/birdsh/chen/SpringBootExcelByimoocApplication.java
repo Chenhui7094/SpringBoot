71caaf4c25b63a77093c995dbf4c6e5b5697b110
chenhuihui <chenhuihui@birdsh.com>
2017-10-17 16:26:06 +0800

    1

---
+++ spring-boot-excel-byimooc/src/main/java/com/birdsh/chen/SpringBootExcelByimoocApplication.java
@@ -0,0 +1,12 @@
+package com.birdsh.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelByimoocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExcelByimoocApplication.class, args);
+	}
+}
