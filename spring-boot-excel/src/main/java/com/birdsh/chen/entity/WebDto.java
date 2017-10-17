4d24b753434754b8fcef57bdddfa3ca7bb30b5b9
chenhuihui <chenhuihui@birdsh.com>
2017-10-17 14:18:19 +0800

    1

---
+++ spring-boot-excel/src/main/java/com/birdsh/chen/entity/WebDto.java
@@ -0,0 +1,40 @@
+package com.birdsh.chen.entity;
+
+public class WebDto {
+
+    //网站名称
+    private String name;
+
+    //网址
+    private String url;
+
+    //用户名
+    private String username;
+
+    //密码
+    private String password;
+
+    //日均访问量
+    private Integer readCount;
+
+    public WebDto(String name, String url, String username, String password, Integer readCount) {
+        this.name = name;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.readCount = readCount;
+    }
+
+    public WebDto() {}
+
+    @Override
+    public String toString() {
+        return "WebDto{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", readCount=" + readCount +
+                '}';
+    }
+}
